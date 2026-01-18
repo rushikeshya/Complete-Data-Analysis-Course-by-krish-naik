47487883e478b732ffcb19ad02d5248e40019fbe
Rushikesh Yadav <rushikeshyadav301220@gmail.com>
2026-01-18 10:57:50 +0530

    Starting Data Analysis with python

---
+++ Section 3 Complete Python with imp libraries/28 Import Modules And Packages In Python/package/maths.py
@@ -0,0 +1,12 @@
+def addition(a,b):
+    return f"The addition of {a} and {b} = {a+b}"
+
+def substraction(a,b):
+    return f"The substraction of {a} and {b} = {a-b}"
+
+def multiplication(a,b):
+    return f"The multiplication of {a} and {b} = {a*b}"
+
+def division(a,b):
+    return f"The division of {a} and {b} = {a/b}"
+
